06e8aafdbe00e1639177862088fd6bbf42b208ee
HB Leang <bunleang2168@gmail.com>
2024-11-23 22:09:14 +0700

    Inventory Entity added

---
+++ IMS_Services/Entities/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_Services.Entities;
+
+public class Inventory
+{
+    public int InvID {get; set; }
+    public decimal UnitCost { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public short CurrentStock { get; set; }
+    public short InitialQty { get; set; }
+    public int ProductID { get; set; }
+    public int ImportID { get; set; }
+
+}

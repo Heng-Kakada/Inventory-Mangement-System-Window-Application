61b70fddf539f7f6c9aceb966c155e0c77462778
Piko Strom <hengkakada899@gmail.com>
2024-11-23 21:26:44 +0700

    previous

--- InventoryManagementSystem/obj/Debug/net8.0-windows/InventoryManagementSystem.AssemblyInfo.cs
+++ InventoryManagementSystem/obj/Debug/net8.0-windows/InventoryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279f0732031904e572c14d6792cbc37af7d95cda")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
